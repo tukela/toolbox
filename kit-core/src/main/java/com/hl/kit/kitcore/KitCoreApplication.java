e3259d588bdce0ddd61027788b26afa23b100eb5
tukela <767224102@qq.com>
2018-09-25 18:51:41 +0800

    2357

---
+++ kit-core/src/main/java/com/hl/kit/kitcore/KitCoreApplication.java
@@ -0,0 +1,12 @@
+package com.hl.kit.kitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitCoreApplication.class, args);
+	}
+}
